db4d1b04ab8b56a7ebafedf0a3783ae212b1b0b7
christa caggiano <christacaggiano@christas-MacBook-Pro.local>
2021-09-10 21:23:36 -0700

    old changes part 2

--- fetch_caravaggio.py
+++ fetch_caravaggio.py
@@ -3,7 +3,6 @@ import requests  # downloads html
 import os
 import random
 
-
 caravaggio = wikipedia.page('List of paintings by Caravaggio')  # get metadata associated with all known caravaggio paintings
 
 # make separate folders for what will be training and test data
@@ -15,18 +14,14 @@ if not os.path.exists("test"):
 total_caravaggio = len(caravaggio.images)  # gets total number of paintings
 total_test = int(total_caravaggio * 0.8)  # determines number that will be allocated to 80% training set
 
-
 # randomly decides what paintings are in training set so not to bias by particular career phase
 random.seed(100)
 test_set = random.sample(range(total_caravaggio), total_test)
 
-
 # downloads and assigns painting to training or test set
 for i, painting in enumerate(caravaggio.images):
-
     if i in test_set:
         f = open("training/" + "Caravaggio_" + str(i) + ".jpg", 'wb')
-
     else:
         f = open("test/" + "Caravaggio_" + str(i) + ".jpg", 'wb')
 
